488be1b70cfce7a6d6d5766f412368aed20c1f4f
Harshavardhan <hvijapuram@gmail.com>
2024-08-07 20:32:44 +0530

    Added Authentication functionality

--- flex/flexapp/urls.py
+++ flex/flexapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
 
     path('',views.CustomLogin,name="login"),
-    path('logout', views.logout_view, name="logout"),
+    path('logout', views.CustomLogout, name="logout"),
     path('dashboard', views.dashboard, name="dashboard"),
 ]
--- flex/flexapp/views.py
+++ flex/flexapp/views.py
@@ -9,15 +9,27 @@ def CustomLogin(request):
         print(user)
         if user is not None:
             login(request, user)
-            return redirect(request,dashboard)
+            return redirect('dashboard')
     return render(request, 'login.html')
 
 def CustomLogout(request):
     logout(request)
-    return redirect(request, login)
+    return redirect('/')
 
 def dashboard(request):
     if request.user.is_authenticated:
         return render(request, 'dashboard.html')
     else:
-        return redirect(request, login)
+        return redirect('/')
+
+def  register(request):
+    if request.method == 'POST':
+        rollno = request.POST.get('rollno')
+        password = request.POST.get('password')
+        first_name = request.POST.get('first_name')
+        is_staff = True if request.POST.get('role') == 'staff' else False
+        user = student.objects.create_user(roll_no=rollno,email=email,is_staff=is_staff,first_name=first_name)
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+    return render(request, 'register.html')
